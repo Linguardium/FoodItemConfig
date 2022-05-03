d7c9d228003c6ea09daf7f47290c5a2b1363a4d5
Linguardium <linguardium@hobbyist.dev>
2022-05-02 19:26:32 -0700

    add the rest of the food component pieces and some commands

--- src/main/java/mod/linguardium/foodconfig/ConfigFile.java
+++ src/main/java/mod/linguardium/foodconfig/ConfigFile.java
@@ -1,13 +1,88 @@
 package mod.linguardium.foodconfig;
 
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ConfigFile {
-    HashMap<String,FoodStats> foodMap = new HashMap<>();
+    HashMap<Identifier,FoodStats> foodMap = new HashMap<>();
     public static class FoodStats {
-        public float saturation = -1f;
-        public int hunger = -1;
+        public Float saturation;
+        public Integer hunger;
+        public Boolean meat;
+        public Boolean alwaysEdible;
+        public Boolean snack;
+        public List<EffectWithChance> effects=new ArrayList<>();
         FoodStats() {}
         FoodStats(int hunger, float saturation) {this.hunger=hunger;this.saturation=saturation;}
+        public static FoodStats fromFoodComponent(FoodComponent component) {
+            FoodStats stat = new FoodStats();
+            if (component != null) {
+                stat.hunger= component.getHunger();
+                stat.saturation=component.getSaturationModifier();
+                stat.meat=component.isMeat();
+                stat.snack=component.isSnack();
+                stat.alwaysEdible=component.isAlwaysEdible();
+                for (Pair<StatusEffectInstance,Float> statusEffect:component.getStatusEffects()) {
+                    stat.effects.add(new EffectWithChance(statusEffect.getFirst(),statusEffect.getSecond()));
+                }
+            }
+            return stat;
+        }
+        public FoodComponent toFoodComponent() {
+            FoodComponent.Builder builder = new FoodComponent.Builder();
+            if (hunger > 0) builder.hunger(hunger);
+            if (saturation>0) builder.saturationModifier(saturation);
+            if (snack) builder.snack();
+            if (meat) builder.meat();
+            if (alwaysEdible) builder.alwaysEdible();
+            for (EffectWithChance effect: effects){
+                builder.statusEffect(effect.effect.toStatusEffectInstance(),effect.chancePercent);
+            }
+            return builder.build();
+        }
+    }
+    public  static class EffectWithChance {
+        StatusEffectInstanceConfig effect;
+        float chancePercent;
+        EffectWithChance(StatusEffectInstance instance,Float chance) {
+            if (instance != null) {
+                effect=StatusEffectInstanceConfig.fromStatusEffectInstance(instance);
+            }
+            chancePercent=(chance!=null)?chance:1.0f;
+        }
+    }
+    public static class StatusEffectInstanceConfig {
+        Identifier type=new Identifier("minecraft:regeneration");
+        int duration=200;
+        int amplifier=1;
+        boolean ambient=false;
+        boolean permanent=false;
+        boolean showParticles = true;
+        boolean showIcon = true;
+        StatusEffectInstance toStatusEffectInstance() {
+            if (type!=null && Registry.STATUS_EFFECT.containsId(type)) {
+                return new StatusEffectInstance(Registry.STATUS_EFFECT.get(type),duration,amplifier,ambient,showParticles,showIcon);
+            }
+            FoodConfig.LOGGER.error("Invalid status effect id {}",type.toString());
+            return null;
+        }
+        public static StatusEffectInstanceConfig fromStatusEffectInstance(StatusEffectInstance instance) {
+            StatusEffectInstanceConfig cfg= new StatusEffectInstanceConfig();
+            cfg.type = Registry.STATUS_EFFECT.getId(instance.getEffectType());
+            cfg.duration=instance.getDuration();
+            cfg.amplifier=instance.getAmplifier();
+            cfg.ambient= instance.isAmbient();
+            cfg.permanent=instance.isPermanent();
+            cfg.showIcon=instance.shouldShowIcon();
+            cfg.showParticles= instance.shouldShowParticles();
+            return cfg;
+        }
     }
 }
--- src/main/java/mod/linguardium/foodconfig/FoodConfig.java
+++ src/main/java/mod/linguardium/foodconfig/FoodConfig.java
@@ -2,15 +2,35 @@ package mod.linguardium.foodconfig;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.mojang.datafixers.util.Pair;
 import mod.linguardium.foodconfig.mixin.FoodComponentAccessor;
 import mod.linguardium.foodconfig.mixin.ItemAccessor;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryRemovedCallback;
+import net.fabricmc.fabric.api.registry.CommandRegistry;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.function.CommandFunction;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Identifier;
+
 import net.minecraft.util.registry.Registry;
+import org.apache.logging.log4j.core.jmx.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,34 +38,63 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FoodConfig implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Food Item Config");
-	public static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().create();
+	public static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().registerTypeAdapter(Identifier.class, new TypeAdapter<Identifier>() {
+		@Override
+		public void write(JsonWriter jsonWriter, Identifier identifier) throws IOException {
+			jsonWriter.value(identifier.toString());
+		}
+
+		@Override
+		public Identifier read(JsonReader jsonReader) throws IOException {
+			return new Identifier(jsonReader.nextString());
+		}
+	}) .create();
+
 	public static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("foodconfig.json");
 	public static ConfigFile config;
+	public static HashMap<Identifier, FoodComponent> FoodStatsDefaults=  new HashMap<>();
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		loadConfig();
-		applyChangesToCurrentItems();
-		RegistryEntryAddedCallback.event(Registry.ITEM).register((rawId, id, item) -> {
-			if (config != null && config.foodMap.containsKey(id.toString())) {
-				LOGGER.info("Modifying {}",id.toString());
-				applyValuesToItem(item,config.foodMap.get(id.toString()));
-			}
+		getItemDefaults();
+
+		// Allows modded status effects to be used. otherwise their identifiers would not be able to be looked up
+		DynamicRegistrySetupCallback.EVENT.register(registryManager -> {
+			applyChangesToCurrentItems();
+		});
+
+		// Allows reload in game if opped or single player
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+
+			dispatcher.register(CommandManager.literal("foodconfig_reload").requires(source->source.hasPermissionLevel(2) || !source.getServer().isDedicated()).executes(context -> {
+				resetItems();
+				loadConfig();
+				applyChangesToCurrentItems();
+				context.getSource().sendFeedback(new LiteralText("Successfully reloaded config!"),false);
+				return 0;
+			}));
+			dispatcher.register(CommandManager.literal("foodconfig_dump").requires(source->source.hasPermissionLevel(2) || !source.getServer().isDedicated()).executes(context -> {
+				dumpComponents();
+				context.getSource().sendFeedback(new LiteralText("Successfully dumped food components!"),false);
+				return 0;
+			}));
 		});
 	}
 	public void loadConfig() {
 		if (!Files.exists(CONFIG_PATH)) {
 			config = new ConfigFile();
-			config.foodMap.put("example:exampleitem",new ConfigFile.FoodStats(1,3.5f));
+
+			ConfigFile.FoodStats stats = ConfigFile.FoodStats.fromFoodComponent(FoodComponents.ENCHANTED_GOLDEN_APPLE);
+			config.foodMap.put(new Identifier("example:exampleitem"),stats);
 			try(FileWriter writer = new FileWriter(CONFIG_PATH.toFile())){
 				writer.write(GSON.toJson(config));
 			} catch (IOException e) {
@@ -63,28 +112,78 @@ public class FoodConfig implements ModInitializer {
 			}
 		}
 	}
+	private static void getItemDefaults() {
+		Registry.ITEM.getIds().forEach(id->{
+			Item item = Registry.ITEM.get(id);
+			if (item.isFood()) {
+				FoodStatsDefaults.put(id, item.getFoodComponent());
+			}
+		});
+		RegistryEntryAddedCallback.event(Registry.ITEM).register((rawId, id, item) -> {
+			if (item.isFood()) {
+				FoodStatsDefaults.put(id, item.getFoodComponent());
+			}
+		});
+		RegistryEntryRemovedCallback.event(Registry.ITEM).register((rawId, id, object) -> {
+			FoodStatsDefaults.remove(id);
+		});
+	}
 	private static void applyValuesToItem(Item item, ConfigFile.FoodStats values) {
 		FoodComponent fc = item.getFoodComponent();
-		if (fc != null) {
-			if (values.saturation>=0) ((FoodComponentAccessor)fc).setSaturationValue(values.saturation);
-			if (values.hunger>=0) ((FoodComponentAccessor)fc).setHungerValue(values.hunger);
-		}else{
-			FoodComponent.Builder builder = new FoodComponent.Builder();
-			if (values.hunger >= 0) {builder.hunger(values.hunger);}
-			if (values.saturation >= 0) {builder.saturationModifier(values.saturation);}
-			fc = builder.build();
+		if (fc == null) {
+			fc = new FoodComponent.Builder().build();
 		}
-		((ItemAccessor)item).setFoodComponent(fc);
+		FoodComponent.Builder builder = new FoodComponent.Builder();
+		if (values.saturation != null) builder.saturationModifier(values.saturation); else builder.saturationModifier(fc.getSaturationModifier());
+		if (values.hunger != null) builder.hunger(values.hunger); else builder.hunger(fc.getHunger());
+		if (	(values.alwaysEdible !=null && values.alwaysEdible) ||
+				(fc.isAlwaysEdible() && values.alwaysEdible == null))
+					builder.alwaysEdible();
+		if (	(values.meat !=null && values.meat) ||
+				(fc.isMeat() && values.meat == null))
+			builder.meat();
+		if (	(values.snack !=null && values.snack) ||
+				(fc.isMeat() && values.snack == null))
+			builder.snack();
+		for (ConfigFile.EffectWithChance effect: values.effects) {
+			builder.statusEffect(effect.effect.toStatusEffectInstance(), effect.chancePercent);
+		}
+		for (Pair<StatusEffectInstance,Float> p: fc.getStatusEffects()) {
+			builder.statusEffect(p.getFirst(),p.getSecond());
+		}
+		((ItemAccessor)item).setFoodComponent(builder.build());
 	}
 	public static void applyChangesToCurrentItems() {
 //		if (config == null)
 //			return;
 		config.foodMap.forEach((key,values)->{
-			Identifier itemId = Identifier.tryParse(key);
-			if (itemId != null && !itemId.getNamespace().equals("example") && Registry.ITEM.containsId(itemId)) {
-				LOGGER.info("Modifying {}",itemId.toString());
-				applyValuesToItem(Registry.ITEM.get(itemId),values);
+			if (!key.getNamespace().equals("example") && Registry.ITEM.containsId(key)) {
+				LOGGER.info("Modifying {}",key.toString());
+				applyValuesToItem(Registry.ITEM.get(key),values);
 			}
 		});
+	}
+	private static void resetItems() {
+		config.foodMap.forEach((key,value)->{
+			if (Registry.ITEM.containsId(key)) {
+				Item item = Registry.ITEM.get(key);
+				((ItemAccessor) item).setFoodComponent(FoodStatsDefaults.getOrDefault(key, null));
+			}
+		});
+	}
+	private static void dumpComponents() {
+		HashMap<Identifier,ConfigFile.FoodStats> map = new HashMap<>();
+		Registry.ITEM.getIds().forEach(id->{
+			Item item = Registry.ITEM.get(id);
+			if (item.isFood()) {
+				map.put(id, ConfigFile.FoodStats.fromFoodComponent(item.getFoodComponent()));
+			}
+		});
+		try(FileWriter writer=new FileWriter(FabricLoader.getInstance().getGameDir().resolve("FoodComponents.json").toFile())) {
+			writer.write(GSON.toJson(map));
+		} catch (IOException e) {
+			LOGGER.error("Failed to write FoodComponents.json to the game directory!");
+			e.printStackTrace();
+		}
 	}
 }
